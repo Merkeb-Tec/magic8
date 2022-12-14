c271d0316a40fac0bbc38b01c361b5acb187b5d5
Merkeb Tecle <merkeb17@gmail.com>
2022-12-13 23:38:23 -0800

    created functions for both the form and the answer div. Creatd an array with the image paths and a mathrandom function that currently doesn't work. Needs attention

--- JS/script.js
+++ JS/script.js
@@ -1,30 +1,61 @@
-function ask () {
+const form = document.forms[0];
+
+const output = document.getElementById('output');
+
+let x = document.getElementById('ask');
+let y = document.getElementById('question');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    output.innerText = `${form.elements.question.value}`;
+    x.style.display = 'none';
+    y.style.display = 'none';
+});
+
+const arr = [
+    arr[0] = "./images/magic8ball_1.png",
+    arr[1] = "./images/magic8ball_2.png",
+    arr[2] = "./images/magic8ball_3.png",
+    arr[3] = "./images/magic8ball_4.png",
+    arr[4] = "./images/magic8ball_5.png",
+    arr[5] = "./images/magic8ball_6.png",
+    arr[6] = "./images/magic8ball_7.png",
+    arr[7] = "./images/magic8ball_8.png",
+    arr[8] = "./images/magic8ball_9.png",
+    arr[9] = "./images/magic8ball_10.png",
+    arr[10] = "./images/magic8ball_11.png",
+    arr[11] = "./images/magic8ball_12.png",
+    arr[12] = "./images/magic8ball_13.png",
+    arr[13] = "./images/magic8ball_14.png",
+    arr[14] = "./images/magic8ball_15.png",
+    arr[15] = "./images/magic8ball_16.png",
+    arr[16] = "./images/magic8ball_17.png",
+    arr[17] = "./images/magic8ball_18.png",
+    arr[18] = "./images/magic8ball_19.png",
+    arr[19] = "./images/magic8ball_20.png"
+]
+
+
+function ask() {
     const question = document.getElementById("question").value;
-    if (question.length < 1) {
-        document.getElementById("answers").innerHTML = "<img src =./magic8/images/magic8ball_extra.png alt=''>";
+    if (question.length === 0) {
+       document.getElementById("answers").innerHTML = "<img src =./magic8/images/magic8ball_extra.png alt=''>";
     } else {
-        let newImage = new Array();
-        newImage[0] = "./magic/magic8ball_1.png"
-        newImage[1] = "./magic/magic8ball_2.png"
-        newImage[2] = "./magic/magic8ball_3.png"
-        newImage[3] = "./magic/magic8ball_4.png"
-        newImage[4] = "./magic/magic8ball_5.png"
-        newImage[5] = "./magic/magic8ball_6.png"
-        newImage[6] = "./magic/magic8ball_7.png"
-        newImage[7] = "./magic/magic8ball_8.png"
-        newImage[8] = "./magic/magic8ball_9.png"
-        newImage[9] = "./magic/magic8ball_10.png"
-        newImage[10] = "./magic/magic8ball_11.png"
-        newImage[11] = "./magic/magic8ball_12.png"
-        newImage[12] = "./magic/magic8ball_13.png"
-        newImage[13] = "./magic/magic8ball_14.png"
-        newImage[14] = "./magic/magic8ball_15.png"
-        newImage[15] = "./magic/magic8ball_16.png"
-        newImage[16] = "./magic/magic8ball_17.png"
-        newImage[17] = "./magic/magic8ball_18.png"
-        newImage[18] = "./magic/magic8ball_19.png"
-        newImage[19] = "./magic/magic8ball_20.png"
-        let random = Math.floor(Math.random()*newImage.length);
-        document.getElementById("answers").innerHTML = "<img src='" + newImage[random] +" 'alt='image'>";
+        let random = [Math.floor(Math.random() * arr.length)];
+        document.getElementById("answers").innerHTML = "<img src='" + arr[random] +" 'alt=''>";
     }
-}
+ };
+
+
+
+//     let one = getElementById("one");
+// let two = getElementById("two");
+// let three = getElementById("three");
+// let four = getElementById("four");
+// let five = getElementById("five");
+// let six = getElementById("six");
+// let seven = getElementById("seven");
+// let eight = getElementById("eight");
+
+
+// const arr = [one,two, three,four,five,six,seven,eight];
