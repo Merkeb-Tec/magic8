440a4f08fe7b1466e8c399af012c993d55ea25fb
Merkeb Tecle <merkeb17@gmail.com>
2022-12-14 19:47:18 -0800

    add the call to function and some last minute styling to background

--- JS/script.js
+++ JS/script.js
@@ -1,61 +1,68 @@
 const form = document.forms[0];
 
-const output = document.getElementById('output');
+const output = document.getElementById("output");
 
-let x = document.getElementById('ask');
-let y = document.getElementById('question');
+let x = document.getElementById("ask");
+let y = document.getElementById("question");
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    output.innerText = `${form.elements.question.value}`;
-    x.style.display = 'none';
-    y.style.display = 'none';
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  output.innerText = `${form.elements.question.value}`;
+  x.style.display = "none";
+  y.style.display = "none";
 });
 
-const arr = [
-    arr[0] = "./images/magic8ball_1.png",
-    arr[1] = "./images/magic8ball_2.png",
-    arr[2] = "./images/magic8ball_3.png",
-    arr[3] = "./images/magic8ball_4.png",
-    arr[4] = "./images/magic8ball_5.png",
-    arr[5] = "./images/magic8ball_6.png",
-    arr[6] = "./images/magic8ball_7.png",
-    arr[7] = "./images/magic8ball_8.png",
-    arr[8] = "./images/magic8ball_9.png",
-    arr[9] = "./images/magic8ball_10.png",
-    arr[10] = "./images/magic8ball_11.png",
-    arr[11] = "./images/magic8ball_12.png",
-    arr[12] = "./images/magic8ball_13.png",
-    arr[13] = "./images/magic8ball_14.png",
-    arr[14] = "./images/magic8ball_15.png",
-    arr[15] = "./images/magic8ball_16.png",
-    arr[16] = "./images/magic8ball_17.png",
-    arr[17] = "./images/magic8ball_18.png",
-    arr[18] = "./images/magic8ball_19.png",
-    arr[19] = "./images/magic8ball_20.png"
-]
+const images = [
+  "../images/magic8ball_1.png",
+  "../images/magic8ball_2.png",
+  "../images/magic8ball_3.png",
+  "../images/magic8ball_4.png",
+  "../images/magic8ball_5.png",
+  "../images/magic8ball_6.png",
+  "../images/magic8ball_7.png",
+  "../images/magic8ball_8.png",
+  "../images/magic8ball_9.png",
+  "../images/magic8ball_10.png",
+  "../images/magic8ball_11.png",
+  "../images/magic8ball_12.png",
+  "../images/magic8ball_13.png",
+  "../images/magic8ball_14.png",
+  "../images/magic8ball_15.png",
+  "../images/magic8ball_16.png",
+  "../images/magic8ball_17.png",
+  "../images/magic8ball_18.png",
+  "../images/magic8ball_19.png",
+  "../images/magic8ball_20.png",
+];
 
+
+const answers = document.getElementById('answers');
+let randomImage = images[Math.floor(Math.random() * images.length)];
+console.log(randomImage)
 
 function ask() {
-    const question = document.getElementById("question").value;
-    if (question.length === 0) {
-       document.getElementById("answers").innerHTML = "<img src =./magic8/images/magic8ball_extra.png alt=''>";
-    } else {
-        let random = [Math.floor(Math.random() * arr.length)];
-        document.getElementById("answers").innerHTML = "<img src='" + arr[random] +" 'alt=''>";
-    }
- };
+  const question = document.getElementById("question").value;
+  if (question.length === 0) {
+    alert('Please Enter a Question');
+  } else {
+      console.log('inside else')
+      const img = document.createElement('img');
+    img.src = randomImage;
+      //"<img src='" + randomImage + " 'alt=''>";
+      answers.appendChild(img);
+  }
+}
+
+document.getElementById('ask').addEventListener('click', ask);
 
 
-
-//     let one = getElementById("one");
-// let two = getElementById("two");
-// let three = getElementById("three");
-// let four = getElementById("four");
-// let five = getElementById("five");
-// let six = getElementById("six");
-// let seven = getElementById("seven");
-// let eight = getElementById("eight");
-
-
-// const arr = [one,two, three,four,five,six,seven,eight];
+// function ask() {
+//     const question = document.getElementById("question").value;
+//     if (question.length === 0) {
+//          alert('Please Enter a Question');
+//          document.getElementById("answers").innerHTML = "<img src =./magic8/images/magic8ball_extra.png alt=''>";
+//     } else {
+//         let random = [Math.floor(Math.random() * images.length)];
+//         document.getElementById("answers").innerHTML = "<img src='" + images[random] +" 'alt=''>";
+//     }
+//  };
